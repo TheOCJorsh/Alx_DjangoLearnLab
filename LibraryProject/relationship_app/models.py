ea48db44a0c74f9143a09f362c4b82a15dbb7d53
TheOCJorsh <ogheghajoshua@gmail.com>
2026-01-14 21:03:33 +0100

    update to Author models

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 # Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -13,8 +13,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         Author,
-        on_delete=models.CASCADE,
-        related_name='books'
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
@@ -24,7 +23,7 @@ class Book(models.Model):
 # Library model
 class Library(models.Model):
     name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book, related_name='libraries')
+    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
@@ -35,22 +34,8 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(
         Library,
-        on_delete=models.CASCADE,
-        related_name='librarian'
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
         return self.name
-
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
