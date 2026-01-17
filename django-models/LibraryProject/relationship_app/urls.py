7ade6750f4b10e21f704a2d2e5f0d1ca14f3b8d1
TheOCJorsh <ogheghajoshua@gmail.com>
2026-01-17 12:36:41 +0100

    Add custom book permissions and secure CRUD views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import delete_book, edit_book, list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
 
 
 urlpatterns = [
@@ -18,6 +19,12 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+
+    # Permission-based URL to protect book operations
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+
 ]
 
     
