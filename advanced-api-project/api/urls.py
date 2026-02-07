8393072cd74cfb8866b9a1f54348382fc1464a53
TheOCJorsh <ogheghajoshua@gmail.com>
2026-02-07 20:11:48 +0100

    updated test view for book class

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,16 +1,21 @@
+"""
 from django.urls import path
-from .views import (
-    BookListView,
-    BookDetailView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView
-)
-
-urlpatterns = [
+from . import views
     path('books/', views.BookListView.as_view(), name='book-list'),  # get all books
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # get one book
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # create new book
     path('books/update/', views.BookUpdateView.as_view(), name='book-update'),  # update book
     path('books/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # delete book
+]
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.BookListView.as_view(), name='book-list'),  # get all books
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # get one book
+    path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # create new book
+    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),  # update book
+    path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),  # delete book
 ]
