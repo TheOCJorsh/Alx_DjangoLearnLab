0a893bf142c24a66112c06fe17efbb2948c3c40f
TheOCJorsh <ogheghajoshua@gmail.com>
2026-02-12 12:15:23 +0100

    blog url update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,12 +15,12 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 
 # URL patterns for blog post operations
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    
+
 ]
 
