735a29823095b5122d34913f64bc299dd32fe046
TheOCJorsh <ogheghajoshua@gmail.com>
2026-02-19 00:11:23 +0100

    added get user model

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
-
+from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
