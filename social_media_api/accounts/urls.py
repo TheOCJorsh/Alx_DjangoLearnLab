aed85bd28e6ae88623285c43b78c8b017bb42237
TheOCJorsh <ogheghajoshua@gmail.com>
2026-02-19 01:18:28 +0100

    Update follow/unfollow to URL patterns

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import follow_user, unfollow_user
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', follow_user, name='follow-user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
     
 ]
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-
-urlpatterns = router.urls
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,6 +6,10 @@ from rest_framework.decorators import action
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, LoginSerializer
 from .models import CustomUser
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 
 User = get_user_model()
 
@@ -81,3 +85,27 @@ class UserViewSet(viewsets.ModelViewSet):
 class UserListView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+
+    if user_to_follow == request.user:
+        return Response(
+            {"error": "You cannot follow yourself"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    request.user.following.add(user_to_follow)
+    return Response({"message": "User followed successfully"})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+
+    request.user.following.remove(user_to_unfollow)
+    return Response({"message": "User unfollowed successfully"})
